86d415249f20d283efc1f743c063f200a39cf7eb
thachpv91 <thachphanvan@gmail.com>
2017-02-16 13:28:40 +0700

    A

---
+++ BreadWallet/NSString+EmailValidation.h
@@ -0,0 +1,13 @@
+//
+//  NSString+EmailValidation.h
+//  LoafWallet
+//
+//  Created by BapVn on 2/16/17.
+//  Copyright © 2017 Aaron Voisine. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (EmailValidation)
+- (BOOL)isValidEmail;
+@end
